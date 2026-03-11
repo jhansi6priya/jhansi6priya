a6b541830c536306ad56dd2787363d80c7e76fda
G Jhansi Priya <148507393+jhansi6priya@users.noreply.github.com>
2026-03-11 19:40:32 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,13 +1,8 @@
 from datetime import datetime
 
 content = f"""
-# Hi, I'm Jhansi 👋
-
-🚀 4th Year BTech Student  
-💻 Learning Python, React, Cloud & DevOps  
-
 ### ⏳ Last Updated
-{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+{datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 Keep building and learning every day ✨
 """

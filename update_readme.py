38f653bc0b4986ddaf2aa3299fa2d82409551127
G Jhansi Priya <148507393+jhansi6priya@users.noreply.github.com>
2026-03-12 08:42:27 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 
-content = f"""
-### ⏳ Last Updated
-{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+with open("README.md", "r") as f:
+    readme = f.read()
 
+new_Date = {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+content = readme.replace(f"
+### ⏳ Last Updated
+{new_Date}
 Keep building and learning every day ✨
-"""
+")
 
 with open("README.md", "w") as f:
     f.write(content)

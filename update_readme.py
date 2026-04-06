28842946109ee79111d8bb5c4015dc9afed28054
G Jhansi Priya <148507393+jhansi6priya@users.noreply.github.com>
2026-04-06 08:52:39 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -5,21 +5,21 @@ with open("README.md", "r") as f:
 
 new_Date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-new_Sec = f"
+new_Sec = f"""
 <!--LAST_UPDATED_START-->
 {new_Date}
 <!--LAST_UPDATED_END-->
-"
+"""
 
 import re
 
 updated_readme = re.sub(
     r"### ⏳ Last Updated.*?Keep building and learning every day ",
-    r"<!--LAST_UPDATED_START-->*?<!--LAST_UPDATED_END-->"
+    r"<!--LAST_UPDATED_START-->*?<!--LAST_UPDATED_END-->",
     new_Sec,
     readme,
     flags=re.DOTALL
 )
 
 with open("README.md", "w") as f:
-    f.write(content)
+    f.write(updated_readme)
